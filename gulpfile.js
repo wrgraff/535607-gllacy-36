05e5525bf54a23625059d9bdc3edde89b050ae99
Artur Trifonov <wrgraff@gmail.com>
2023-05-07 18:42:22 +0400

    Вносит поправки в настройку сборки

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ import plumber from "gulp-plumber";
 import postcss from "gulp-postcss";
 import autoprefixer from "autoprefixer";
 import csso from "postcss-csso";
+import pimport from "postcss-import";
 import rename from "gulp-rename";
 import { stacksvg } from "gulp-stacksvg";
 import svgo from "gulp-svgmin";
@@ -12,68 +13,69 @@ import browser from "browser-sync";
 const { src, dest, watch, series, parallel } = gulp;
 
 export function processStyles () {
-	return src(["./styles/**/*.css", "!./styles/**/*.min.css"])
-		.pipe(plumber())
-		.pipe(postcss([
-			autoprefixer(),
-			csso()
-		]))
-		.pipe(
-			rename({
-				extname: ".min.css"
-			})
-		)
-		.pipe(dest("./styles/"))
-		.pipe(browser.stream());
+  return src("./css/style.css")
+    .pipe(plumber())
+    .pipe(postcss([
+      pimport(),
+      autoprefixer(),
+      csso()
+    ]))
+    .pipe(
+      rename({
+        extname: ".min.css"
+      })
+    )
+    .pipe(dest("./css/"))
+    .pipe(browser.stream());
 }
 
 export function createStack () {
-	return src(["./icons/**/*.svg", "!./icons/stack.svg"])
-		.pipe(svgo())
-		.pipe(stacksvg())
-		.pipe(dest("./icons"));
+  return src(["./icons/**/*.svg"])
+    .pipe(svgo())
+    .pipe(stacksvg())
+    .pipe(dest("./img"));
 }
 
 export function validateMarkup() {
-	return src("./*.html")
-		.pipe(htmlValidator.analyzer())
-		.pipe(htmlValidator.reporter({ throwErrors: true }))
+  return src("./*.html")
+    .pipe(htmlValidator.analyzer())
+    .pipe(htmlValidator.reporter({ throwErrors: true }))
 }
 
 export function startServer (done) {
-	browser.init({
-		server: {
-			baseDir: "./"
-		},
-		cors: true,
-		notify: false,
-		ui: false,
-	});
-	done();
+  browser.init({
+    server: {
+      baseDir: "./"
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+  done();
 }
 
 function reloadServer (done) {
-	browser.reload();
-	done();
+  browser.reload();
+  done();
 }
 
 function watchFiles () {
-	watch(["./styles/**/*.css", "!./styles/**/*.min.css"], series(processStyles));
-	watch(["./icons/**/*.svg", "!./icons/stack.svg"], series(createStack, reloadServer));
-	watch("./**/*.{html,js,jpg,png,svg,ico,webmanifest}", series(reloadServer));
+  watch(["./css/**/*.css", "!./css/**/*.min.css"], series(processStyles));
+  watch(["./icons/**/*.svg"], series(createStack, reloadServer));
+  watch("./**/*.{html,js,jpg,png,svg,ico,webmanifest}", series(reloadServer));
 }
 
 export function compileProject (done) {
-	parallel(
-		processStyles,
-		createStack
-	)(done);
+  parallel(
+    processStyles,
+    createStack
+  )(done);
 }
 
 export function runDev (done) {
-	series(
-		compileProject,
-		startServer,
-		watchFiles
-	)(done);
+  series(
+    compileProject,
+    startServer,
+    watchFiles
+  )(done);
 }
